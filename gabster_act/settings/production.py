c0b78824f4dfa7d81ed58f872677b94393118967
Jodi Alcantara <jjalcantara1@student.hau.edu.ph>
2023-11-14 06:51:27 +0800

    again

--- gabster_act/settings/production.py
+++ gabster_act/settings/production.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-qgxllxkhe6i=wgq9sr^&vl$uo67i4q=*(y%ea&f(=v*)5-41z8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # '.herokuapp.com','*',
 # Application definition
