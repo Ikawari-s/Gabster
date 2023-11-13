c9fb623dfd253ea75a25c81511f457cfd83e500a
Jodi Alcantara <jjalcantara1@student.hau.edu.ph>
2023-11-14 07:14:06 +0800

    again

--- gabster_act/settings/production.py
+++ gabster_act/settings/production.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure-qgxllxkhe6i=wgq9sr^&vl$uo67i4q=*(y%ea&f(=v*)5-41z8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['gabster2-c5095d821f4f.herokuapp.com']
 
