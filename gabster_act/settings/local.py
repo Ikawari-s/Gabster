fde919e5cb3eef1c8935622af5cffabb93bc6693
Jodi Alcantara <jjalcantara1@student.hau.edu.ph>
2023-11-14 13:04:11 +0800

    asdf

--- gabster_act/settings/local.py
+++ gabster_act/settings/local.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'contact',
     'customization',
     'comment',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -140,7 +141,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'media_root')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+from gabster_act.aws.conf import *
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # LOGIN_REDIRECT_URL = 'profile'
